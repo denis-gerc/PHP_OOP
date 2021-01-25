31c07adb17ebe6011166cb8dd9e2579dbaea50bf
denis-gerc <gdv1001@gmail.com>
2021-01-25 16:56:27 +0300

    end lesson-4

--- classes/Worker.php
+++ classes/Worker.php
@@ -6,6 +6,13 @@ class Worker
 	public $name;
 	public $salary;
 
+	public function __construct($age, $name, $salary)
+	{
+		$this->age = $age;
+		$this->name = $name;
+		$this->salary = $salary;
+	}
+
 	//Вариант 1
 	public function checkAge()
 	{
@@ -21,7 +28,7 @@ class Worker
 	// Вариант 2
 	public function checkAge2()
 	{
-		$valid = $this->age >= 17 & $this->age <= 70 ? 'Валидный возраст' : 'Не валидный возраст';
+		$valid = $this->age >= 17 & $this->age <= 70 ? 'Валидный возраст ' : 'Не валидный возраст ';
 		return $valid;
 	}
 
@@ -30,15 +37,15 @@ class Worker
 	{
 		$minmaxAge = range(18, 70);
 		foreach ($minmaxAge as $key) {
-			if ($var === $key) return 'Валидный возраст';
+			if ($var === $key) return 'Валидный возраст ';
 		};
-		return 'Не валидный возраст';
+		return 'Не валидный возраст ';
 	}
 
 	// Вариант 4
 	public function checkAge4($var)
 	{
 		$valueToKey = array_flip(range(18, 70));
-		return array_key_exists($var, $valueToKey) ? 'Валидный возраст' : 'Не валидный возраст';
+		return array_key_exists($var, $valueToKey) ? 'Валидный возраст ' : 'Не валидный возраст ';
 	}
 }
--- index.php
+++ index.php
@@ -9,16 +9,11 @@ function debug($data)
 	echo '</pre>';
 }
 
-$obj1 = new Worker();
-$obj1->name = 'Иван';
-$obj1->age = 18;
-$obj1->salary = 1000;
+$obj1 = new Worker(17, 'Иван', 1000);
 
-$obj2 = new Worker();
-$obj2->name = 'Вася';
-$obj2->age = 71;
-$obj2->salary = 2000;
+$obj2 = new Worker(71, 'Вася', 2000);
 
-echo $obj1->checkAge4($obj1->age);
+
+echo $obj1->checkAge4($obj1->age) . $obj1->name;
 echo '<hr>';
-echo $obj2->checkAge4($obj2->age);
+echo $obj2->checkAge4($obj2->age) . $obj2->name;
